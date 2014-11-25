2090b843a85ec7d6f9b5f9d86b71c13995d48fdd
Luthaf <luthaf@luthaf.fr>
2014-11-25 11:16:58 +0100

    Fix freezing on Windows

--- setup.py
+++ setup.py
@@ -31,13 +31,13 @@ options = {
 py2app_options = {
     'argv_emulation': True,
     'includes': ["PySide.QtXml"],
-    'packages': ["xml", "markdown"],
+    'packages': ["xml", "markdown", "pygments"],
     'iconfile': "assets/img/clem.icns",
 }
 
 py2exe_options = {
     'includes': ["PySide.QtXml"],
-    'packages': ["xml", "markdown"],
+    'packages': ["xml", "markdown", "pygments"],
     'dist_dir': os.path.join(os.path.dirname(__file__), "dist", "windows"),
     'excludes': ['_ssl'],
     'bundle_files': 3, # Should be keeped, needed by PySide
